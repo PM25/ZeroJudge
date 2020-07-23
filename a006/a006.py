4327ee6ca32e656e7601d70f26bd551db8906c98
HSU0 <64117342+HSU0@users.noreply.github.com>
2020-07-23 15:33:37 +0800

    done

--- a006/a006.py
+++ a006/a006.py
@@ -1,5 +1,18 @@
 import sys
+import math
 
 for line in sys.stdin:
     a, b, c = map(int, line.split(" "))
-    pass
+    if b**2-4*a*c<0:
+        print("No real root")
+    else:
+        x=(-b+math.sqrt(b**2-4*a*c))/(2*a)
+        y=(-b-math.sqrt(b**2-4*a*c))/(2*a)
+        if x==y:
+            print("Two same roots x="+str(int(x)))
+        if x!=y:
+            print("Two different roots x1="+str(int(x))+" , x2="+str(int(y)))
+
+    
+     
+    
--- a009/a009.py
+++ a009/a009.py
@@ -1,4 +1,17 @@
+#%%
 import sys
 
 for line in sys.stdin:
-    pass
+    for cc in line:
+      a=ord(cc)-7
+      print(chr(a), end='')
+    print()
+
+      
+      
+
+
+
+
+
+# %%
